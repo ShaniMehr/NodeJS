07a4b5dda29aa1aeedf48c39c86b8aa830d23108
unknown <zeeshan@gameshock.io>
2024-03-12 10:45:15 +0500

    up

--- server.mjs
+++ server.mjs
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve your Unity game files (build) here
+
 app.use(express.static("https://shanimehr.github.io/WEBGL_GAME/"));
 
 // Handle other routes or API endpoints if needed
 app.get("/", (req, res) => {
-    res.send("Welcome to my Unity game server!");
+   // res.send("Welcome to my Unity game server!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    //console.log(`Server is running on port ${port}`);
 });
